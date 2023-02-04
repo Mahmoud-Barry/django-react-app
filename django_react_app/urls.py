212fbfa18eb73eb68fe43f3964204b71a812a2eb
Mahmoud-Barry <mahmoudbarrysn@gmail.com>
2023-02-04 18:45:25 +0000

    fixed 404 page not found

--- django_react_app/urls.py
+++ django_react_app/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
 from .views import indexView 
 
-urlpatterns = [
-    path('', indexView), 
+
+# add your react routes paths here
+REACT_ROUTES = [
+    'home', #like this
 ]
+
+urlpatterns = [
+    path('', indexView,name="index"), 
+]
+
+for route in REACT_ROUTES:
+    urlpatterns += [
+        path(f"{route}", indexView, name="index")
+    ]
